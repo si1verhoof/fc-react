4b2640b3322c9e6448ea48cb8764398282526e8f
si1verhoof <ftadsh@gmail.com>
2018-08-02 20:52:42 +0500

    Оформлена форма входа

--- js/index.js
+++ js/index.js
@@ -57,7 +57,53 @@ class RegForm extends React.Component {
   }
 }
 
+class LoginForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "",
+      loginPass: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  formSubmit(event) {
+    event.preventDefault(); 
+    const { elements } = event.target;
+    const { login, loginPass } = elements;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.formSubmit}>
+        <label>
+          Введите логин:  
+          <input type="text" name="login" required placeholder="Введите логин" value={this.state.login} onChange={this.handleInputChange}/>
+        </label>
+        <label>
+          Введите пароль:
+          <input type="password" name="loginPass" required placeholder="Введите пароль" value={this.state.loginPass} onChange={this.handleInputChange}/>
+        </label>
+        <button type="submit" value="Войти" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
   <RegForm />,
+  <LoginForm />,
   document.getElementById('root')
 );
