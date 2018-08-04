ff3ab42a5b1dcfbfc13fc4cb218b6281f57012dd
si1verhoof <ftadsh@gmail.com>
2018-08-04 10:16:34 +0500

    попытка настроить контейнер app для рендера компонентов

--- js/components/App.js
+++ js/components/App.js
@@ -1,4 +1,24 @@
 import React from "react";
-import RegForm from "./RegForm"
-import LoginForm from "./LoginForm"
+import RegForm from "./RegForm";
+import LoginForm from "./LoginForm";
+// import { Switch, Route } from 'react-router';
+
+/*
+<Switch>
+  <Route exact path="/" component={Home}/>
+  <Route path="/RegForm" component={RegForm}/>
+  <Route path="/LoginForm" component={LoginForm}/>
+</Switch>
+*/
+
+class App extends React.Component {
+    render () {
+        return ( 
+            <div> 
+                <RegForm />
+                <LoginForm />
+            </div>
+       );
+    }
+}
 
